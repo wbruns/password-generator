209cf482cd07e2362c52779ddbd16812fb1e04ea
wbruns <wbruns07@gmail.com>
2022-01-15 19:09:13 -0600

    created arrays for different character types, the selected types push their contents to selectedCriteria for generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,17 @@
 // display password
 // Assignment code here
 
-// array for selected criteria
+// object for selected criteria
 var selectedCriteria = [];
+// lowercase characters
+var arrayLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// uppercase characters
+var arrayUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// numbers
+var arrayNum = ['0','1','2','3','4','5','6','7','8','9'];
+// special characters
+var arraySpecial = ['!','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','}','~'];
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -33,18 +42,37 @@ var generatePassword = function() {
   } else {
     promptCriteria();
   }
+  // start generating characters 
+
 };
 
+// confirm character types for the password
 var promptCriteria = function () {
-  var confirmUpper = confirm("Would you like to include Uppercase letters?");
-  console.log(confirmUpper);
-
+  var confirmLower = confirm("Would you like to include lowercase letters?");
+  // if yes, add arrayLower to selectedCriteria
+  if (confirmLower) {
+    selectedCriteria.push(...arrayLower);
+  }
+  var confirmUpper = confirm("Would you like to include uppercase letters?");
+  // if yes, add arrayUpper to selectedCriteria
+  if (confirmUpper) {
+    selectedCriteria.push(...arrayUpper);
+  }
   var confirmNumeric = confirm("Would you like to include numbers?");
-  console.log(confirmNumeric);
-
+  // if yes, add arrayNum to selectedCriteria
+  if (confirmNumeric) {
+    selectedCriteria.push(...arrayNum);
+  }
   var confirmSpecial = confirm("Would you like to include special characters?");
-  console.log(confirmSpecial);
-
+  // if yes, add arraySpecial to selectedCriteria
+  if (confirmSpecial) {
+    selectedCriteria.push(...arraySpecial);
+  }
+  if (!confirmLower && !confirmUpper && !confirmNumeric && !confirmSpecial) {
+    alert("Please choose at least one of the options for your password.");
+    promptCriteria();
+  }
+  console.log(selectedCriteria);
   
 };
 
